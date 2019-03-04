41ac5aa3ae0a4bd3973faf9f740758ff1eda331b
Ryan Pethel <arpethel@gmail.com>
2019-03-04 23:08:46 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -154,20 +154,22 @@ def team_names(game_hash)
   end)
 end
 
-
-# def team_names(hash)
-#   # game_hash.each do |which_team, team_info|
-#   #   # puts (team_info[:team_name].colect do |team| team end)
-#   #   puts team_info[:team_name]
-#   # end
-# end
-
 def player_numbers(passed_name)
   game_hash.each do |home_away, team_info|
     if team_info[:team_name] == passed_name
       return (team_info[:players].collect do |player, player_info|
         player_info[:number]
       end)
+    end
+  end
+end
+
+def player_stats(player_name)
+  game_hash.each do |home_away, team_info|
+    team_info[:players].each do |player, player_info|
+      if player == player_name
+        return player_info
+      end
     end
   end
 end
@@ -186,24 +188,3 @@ end
 
 
 
-
-
-
-
-# def good_practices
-#   game_hash.each do |location, team_data|
-#     #are you ABSOLUTELY SURE what 'location' and 'team data' are? use binding.pry to find out!
-#     binding.pry
-#       team_data.each do |attribute, data|
-#         #are you ABSOLUTELY SURE what 'attribute' and 'team data' are? use binding.pry to find out!
-#         binding.pry
-#
-#         #what is 'data' at each loop through out .each block? when will the following line of code work and when will it break?
-#         data.each do |data_item|
-#             binding.pry
-#       end
-#     end
-#   end
-# end
-#
-# good_practices
