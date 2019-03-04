8cad002b13f6ead8868a6c577662cc60ea7897a5
Ryan Pethel <arpethel@gmail.com>
2019-03-04 21:42:05 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -150,7 +150,8 @@ end
 # Returning an array
 def team_names(game_hash)
   game_hash.each do |which_team, team_info|
-    puts (team_info[:team_name].colect do |team| team end)
+    # puts (team_info[:team_name].colect do |team| team end)
+    puts team_info
   end
 end
 
