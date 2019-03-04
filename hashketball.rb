4d4cf04bf4b685e32491dbc24d0234cd13532b16
Ryan Pethel <arpethel@gmail.com>
2019-03-04 23:58:49 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -149,8 +149,8 @@ end
 
 # Returning an array
 def team_names(game_hash)
-  game_hash.each do |home_away, team_info|
-    return team_info[:team_name].collect {|name| name}
+  game_hash.collect do |home_away, team_info|
+    team_info[:team_name]
   end
 end
 
